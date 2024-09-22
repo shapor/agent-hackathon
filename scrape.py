bf1708d10c9592a1aacd23e9646622a31555e538
Shapor Naghibzadeh <shapor@gmail.com>
2024-09-22 11:41:47 -0700

    scrolling working properly but not waiting for more

--- scrape.py
+++ scrape.py
@@ -2,10 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
 import re
+import sys
 
 def address_Addition(address1, new_df):
     address_list = address1.split(',')
@@ -22,7 +25,7 @@ def get_review_summary(result_set):
     for result in result_set:
         review_name = result.find(class_='d4r55').text
         review_text = result.find('span', class_='wiI7pd').text
-        
+
         # Extract the number of stars
         stars_element = result.find('span', class_='kvMYJc')
         stars = 0
@@ -31,7 +34,7 @@ def get_review_summary(result_set):
             stars_match = re.search(r'(\d+) stars?', aria_label)
             if stars_match:
                 stars = int(stars_match.group(1))
-        
+
         rev_dict['Review Name'].append(review_name)
         rev_dict['Review Text'].append(review_text)
         rev_dict['Stars'].append(stars)
@@ -52,8 +55,8 @@ for i, single_url in enumerate(url):
     address = response.find('div', class_='rogA2c').text
     print(f"found address {address}")
 
-    SCROLL_PAUSE_TIME = 5
-    scroll_attempts = 5
+    SCROLL_PAUSE_TIME = 2
+    MAX_SCROLL_ATTEMPTS = 50
 
     # Find and click the "Reviews" tab
     try:
@@ -63,24 +66,51 @@ for i, single_url in enumerate(url):
         reviews_tab.click()
         time.sleep(3)
     except Exception as e:
-        print(f"Error clicking Reviews tab: {e}")
+        print(f"Error clicking Reviews tab: {e}", file=sys.stderr)
         continue
 
     # Scroll through reviews
-    for _ in range(scroll_attempts):
-        print(f"scrolling...")
-        ele = driver.find_element(By.XPATH, '//div[contains(@class, "m6QErb")]')
-        driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', ele)
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    scroll_attempts = 0
+    no_change_count = 0
+
+    while scroll_attempts < MAX_SCROLL_ATTEMPTS:
+        print(f"scrolling... (attempt {scroll_attempts + 1})", file=sys.stderr)
+        scroll_attempts += 1
+
+        # Try to find the scrollable element
+        try:
+            scrollable_div = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@role='main']//div[contains(@class, 'DxyBCb')]"))
+            )
+            driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_div)
+        except:
+            print("Could not find scrollable element", file=sys.stderr)
+            # If we can't find the scrollable div, try scrolling the whole page
+            driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+
         time.sleep(SCROLL_PAUSE_TIME)
 
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            no_change_count += 1
+            if no_change_count >= 3:  # If no change for 3 consecutive attempts, assume we've reached the bottom
+                print("Reached the bottom of the page", file=sys.stderr)
+                break
+        else:
+            no_change_count = 0  # Reset the counter if the height changed
+
+        last_height = new_height
+
     # Click "More" buttons
-    more_buttons = driver.find_elements(By.XPATH, '//button[contains(., "More")]')
-    for button in more_buttons:
-        try:
-            button.click()
-            time.sleep(1)
-        except:
-            pass
+    print("clicking more...", file=sys.stderr)
+    try:
+        more_buttons = driver.find_elements(By.XPATH, '//button[contains(@class, "w8nwRe") and contains(., "More")]')
+        for button in more_buttons:
+            driver.execute_script("arguments[0].click();", button)
+            time.sleep(0.5)
+    except Exception as e:
+        print(f"Error clicking 'More' buttons: {e}", file=sys.stderr)
 
     # Extract reviews
     response = BeautifulSoup(driver.page_source, 'html.parser')
